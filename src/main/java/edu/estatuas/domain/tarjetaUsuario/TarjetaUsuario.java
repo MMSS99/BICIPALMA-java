b546fdd64d329cbfebddeca98f849638944024aa
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-02-17 21:46:08 +0100

    feat(Estacion): :sparkles: create TarjetaUsuario object and methods

--- src/main/java/edu/estatuas/domain/tarjetaUsuario/TarjetaUsuario.java
+++ src/main/java/edu/estatuas/domain/tarjetaUsuario/TarjetaUsuario.java
@@ -1,4 +1,27 @@
 package edu.estatuas.domain.tarjetaUsuario;
 
 public class TarjetaUsuario {
+    private final String id;
+    private boolean activada;
+
+    public TarjetaUsuario(String id, boolean estado){
+        this.id = id;
+        this.activada = estado;
+    }
+
+    public boolean isActivada() {
+        return activada;
+    }
+
+    public void setActivada(boolean nuevoEstado) {
+        this.activada = nuevoEstado;
+    }
+
+    @Override
+    public String toString() {
+        return "TarjetaUsuario{" +
+                "id='" + id + '\'' +
+                ", activada=" + activada +
+                '}';
+    }
 }
