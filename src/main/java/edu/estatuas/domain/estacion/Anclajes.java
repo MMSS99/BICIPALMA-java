0900a9d974fb36d399d4ee40b881a7e198359cdb
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-02-17 15:13:54 +0100

    feat(Anclajes): :sparkles: add Anclajes constructor

--- src/main/java/edu/estatuas/domain/estacion/Anclajes.java
+++ src/main/java/edu/estatuas/domain/estacion/Anclajes.java
@@ -1,4 +1,21 @@
 package edu.estatuas.domain.estacion;
 
 public class Anclajes {
+
+    private final Anclajes[] anclajes;
+
+    Anclajes(int cantidadAnclajes){
+        this.anclajes = new Anclajes[cantidadAnclajes];
+    }
+    private void crearAnclajes(){
+
+    }
+
+    Anclaje[] anclajes(){
+
+    }
+
+    int numAnclajes(){
+        return
+    }
 }
