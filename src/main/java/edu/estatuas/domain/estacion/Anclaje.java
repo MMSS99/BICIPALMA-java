9d5543e3b07a2c056d8e4d3b61d0fa2d97367860
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-02-17 21:07:41 +0100

    feat(Anclaje): :sparkles: add Anclaje object and simple methods (generated almost in it's interity by IntelliJ)

--- src/main/java/edu/estatuas/domain/estacion/Anclaje.java
+++ src/main/java/edu/estatuas/domain/estacion/Anclaje.java
@@ -1,4 +1,36 @@
 package edu.estatuas.domain.estacion;
 
+import edu.estatuas.domain.bicicleta.Movil;
+
 public class Anclaje {
+    private boolean ocupado;
+    private Movil bici;
+
+    Anclaje() {
+        this.ocupado = false;
+        this.bici = null;
+    };
+
+    public boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    public Movil getBici() {
+        return bici;
+    }
+
+    public void anclarBici(Movil bici) {
+        this.bici = bici;
+        this.ocupado = true;
+    }
+
+    public void liberarBici(){
+        this.bici = null;
+        this.ocupado = false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
