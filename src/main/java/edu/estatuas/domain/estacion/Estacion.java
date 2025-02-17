2e08aa5f1b36326bab35424f2a8f22721e9ddce2
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-02-17 14:55:44 +0100

    feat(Estacion): :sparkles: add id and direccion getters

--- src/main/java/edu/estatuas/domain/estacion/Estacion.java
+++ src/main/java/edu/estatuas/domain/estacion/Estacion.java
@@ -1,4 +1,27 @@
 package edu.estatuas.domain.estacion;
 
 public class Estacion {
+    private final int id;
+    private final String direccion;
+    private final int numeroAnclajes;
+
+     Estacion(int id, String direccion, int numeroAnclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.numeroAnclajes = numeroAnclajes;
+    }
+
+    private int getId(){
+         return this.id;
+    }
+
+    private String getDireccion(){
+         return this.direccion;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
